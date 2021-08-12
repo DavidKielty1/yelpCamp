afc80acd543e5e383b9058934b14a759abbc9e17
DavidKielty1 <davidkielty@live.co.uk>
2021-08-12 12:31:03 +0100

    Added review authorization, isReviewAuthor, changed show.ejs

--- app.js
+++ app.js
@@ -92,6 +92,6 @@ app.use((err, req, res, next) => {
   res.status(statusCode).render("error", { err });
 });
 
-app.listen(2000, () => {
-  console.log("Serving on port 2000");
+app.listen(3000, () => {
+  console.log("Serving on port 3000");
 });
